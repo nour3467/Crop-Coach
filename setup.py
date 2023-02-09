6d0cd6569917756a30db1fb11f1bf13be75e85d4
nour3467 <noureddineechchouky@gmail.com>
2023-02-09 17:25:15 +0100

    Adding the traitlets_pcse package to required dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = (
     "crop_coach : A crop growth simulation model turn into OpenAI Gym environment"
 )
@@ -30,7 +30,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["gym", "shutup"],
+    install_requires=["gym", "shutup", "traitlets_pcse"],
     keywords=[
         "python",
         "Reinforcement learning",
